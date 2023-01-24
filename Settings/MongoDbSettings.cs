29be2ffca7723f4534c2b5ea661f074d83cbcea0
Anti Coding Club <73062879+Thoybur-Rohman@users.noreply.github.com>
2023-01-24 23:53:09 +0000

    updated crud calls

---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,17 @@
+namespace Catalog.Setttings
+{
+    public class MongoDbSettings{
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string  ConnectionString { 
+            get
+            {
+                return $"mongodb://{Host}:{Port}";
+            }
+        
+        }
+    }
+}
